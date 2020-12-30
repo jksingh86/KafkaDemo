79a437e44100a76711ee8e45739f14183a60fb73
jksingh86 <with.jiten@gmail.com>
2020-12-30 22:12:44 +0530

    FirstTemplate Commit

---
+++ src/main/java/com/demos/async/kafka/processor/ProcessorAppApplication.java
@@ -0,0 +1,13 @@
+package com.demos.async.kafka.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demos/async/kafka/processor/ProcessorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demos.async.kafka.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
